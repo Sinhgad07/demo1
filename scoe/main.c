6a7b03d0b0daa1966bce6e9b5c97c97c679fa4fe
Poulami1903 <ghoshpoulami1903@gmail.com>
2023-04-18 18:57:40 +0530

    ADD bubble and insertion sort

--- scoe/main.c
+++ scoe/main.c
@@ -25,12 +25,12 @@ int main()
 		case 2:
 			bubble();
 			break;
-		case 3:
+		/*case 3:
 			insertion_sort();
 			break;
 		case 4:
 			merge();
-			break;
+			break;*/
 		case 0:
 			exit(0);
 			break;
@@ -49,5 +49,49 @@ void input_data()
 	for (i=0;i<n;i++)
 	{
 		scanf("%d", &arr[i]);
+	}
+}
+void bubble()
+{
+	int i, n, temp, j, arr[10];
+	printf("\n Enter the number of elements in the array : ");
+	scanf_s("%d", &n);
+	printf("\n Enter the elements: ");
+	for (i = 0; i < n; i++)
+	{
+		scanf_s("%d", &arr[i]);
+	}
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < (n - 1 - i); j++)
+		{
+			if (arr[j] > arr[j + 1])
+			{
+				temp = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = temp;
+			}
+		}
+	}
+	printf("\n The array sorted in ascending order is :\n");
+	for (i = 0; i < n; i++)
+		printf("%d\t", arr[i]);
+	getchar();
+	return 0;
+}
+
+void insertion_sort(int arr[], int n)
+{
+	int i, j, temp;
+	for (i = 1; i < n; i++)
+	{
+		temp = arr[i];
+		j = i - 1;
+		while ((temp < arr[j]) && (j >= 0))
+		{
+			arr[j + 1] = arr[j];
+			j--;
+		}
+		arr[j + 1] = temp;
 	}
 }
